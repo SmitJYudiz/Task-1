f80b3fe4a56136651c1c286cba2c234ae9b2260e
Smit J <smit.patel@yudiz.com>
2022-05-06 13:18:24 +0530

    final commit

--- Assets/Resources/Task 2/GetDataBehaviour.cs
+++ Assets/Resources/Task 2/GetDataBehaviour.cs
@@ -4,59 +4,56 @@ using System.Linq;
 using UnityEngine;
 public class GetDataBehaviour : MonoBehaviour
 {
-    //loading assets from the Resources folder using the generic Resources.Load<T>(path) method
+   
     public TextAsset textFile;
-    public int numberOfWords;
+    //public int numberOfWords;
     public string startingLetter;
 
-    //ek nae list me needed words dalo
-    [HideInInspector]    
-    public List<string>  rawWords;
+    public int wordLength;
 
+    //add all the words of the file to the below list
     [HideInInspector]
-    public int counter = 0;
+    public List<string> rawWords;
+
+    bool anyWordsFound;
+
+
 
     private void Awake()
     {
 
-
-
-        // textFile = Resources.Load<TextAsset>("Assets/Resources/Task 2/Text/words_alpha");
-        //above line do not gives us required results.... and as we are having a placeholder in inspector to put the textfile, we are directly putting it there
-        rawWords = textFile.text.Split('\n').ToList();
-
+        
 
         if (textFile != null)
         {
-            foreach (string word in rawWords)
-            {
-                if (word.StartsWith(startingLetter))
-                {
-                    Debug.Log(word);
-                    counter++;
-                    if (counter >= numberOfWords)
-                    {
-                        break;
-                    }
-                }
-            }
-        
-            //below while loop code just crashes unity
-            //int i = 0;
-            /* while (i < numberOfWords)
-             {
-                 if(rawWords[i].StartsWith(startingLetter))
-                 {
-                     Debug.Log(rawWords[i]);
-                     i++;
-                 }
-             }*/
-
-            
+            rawWords = textFile.text.Split().ToList();
+            // textFile = Resources.Load<TextAsset>("Assets/Resources/Task 2/Text/words_alpha");
+            //above line do not gives us required results.... and as we are having a placeholder in inspector to put the textfile, we are directly putting it there
         }
         else
         {
             Debug.Log("textFile is  null");
         }
     }
+
+    private void Start()
+    {
+        anyWordsFound = false;
+
+
+        foreach (string word in rawWords)
+        {
+            if (word.StartsWith( startingLetter) && word.Length == wordLength)
+            {
+                Debug.Log(word);
+                anyWordsFound = true;
+            }
+        }
+
+        if(anyWordsFound==false)
+        {
+            Debug.Log("no words found for the given parameters");
+        }
+    }
+
 }
